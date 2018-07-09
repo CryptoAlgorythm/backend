b8217871ca4e943fcc7d0469f879b2637b149b68
cryptoalgorythm <cryptoalgorythm@gmail.com>
2018-07-09 00:59:01 -0400

    added route for front-end upload file functionality

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/upload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
